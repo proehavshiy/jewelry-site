a7640eb084154460f35a98ea1ad5204936bbc598
Egor Kurkin <proehavshiynapravo@yandex.ru>
2022-05-08 19:39:15 +0300

    fix: layout bugs

--- src/htmlParts/components/Footer/Footer.js
+++ src/htmlParts/components/Footer/Footer.js
@@ -3,11 +3,7 @@ export default () => {
   const adressesBlock = document.querySelector('.footer__adresses_list')
   const STATUSCLASS = 'footer__adresses_opened'
 
-  console.log('консоль:', button);
-
   button.addEventListener('click', () => {
-    console.log('adressesBlock.style.display:', adressesBlock.classList.contains(STATUSCLASS));
-
     adressesBlock.classList.toggle(STATUSCLASS)
   })
 }
